73ca30f05095899a9991db37dcfeb4e68adca157
Sal Niro <scniro@outlook.com>
2017-06-30 14:25:37 -0400

    dep updates

--- index.js
+++ index.js
@@ -7,42 +7,42 @@ var tpl2js = require('angular-tpl2js');
 
 module.exports = function gulpAngularTpl2js(options) {
 
-    options = options || {};
+  options = options || {};
 
-    return new Transform({
-        objectMode: true,
-        transform: function modifyContents(file, enc, cb) {
+  return new Transform({
+    objectMode: true,
+    transform: function modifyContents(file, enc, cb) {
 
-            var self = this;
+      var self = this;
 
-            var run = new VinylBufferStream(function (buf, done) {
-                var fileOptions = objectAssign({target: file.path, gulp: true}, options);
+      var run = new VinylBufferStream(function (buf, done) {
+        var fileOptions = objectAssign({target: file.path, gulp: true}, options);
 
-                if (fileOptions.relativeTo === undefined && (fileOptions.root || file.path)) {
-                    fileOptions.relativeTo = path.dirname(path.resolve(options.root || file.path));
-                }
-
-                tpl2js.inline(buf, fileOptions || {}, function (err, result) {
-
-                    if(err) {
-                        done(err);
-                        return;
-                    }
-
-                    done(null, new Buffer(result));
-                });
-            });
-
-            run(file, function (err, contents) {
-
-                if (err) {
-                    self.emit('error', new PluginError('gulp-angular-tpl2js', err, {fileName: file.path}));
-                } else {
-                    file.contents = contents;
-                    self.push(file);
-                }
-                cb();
-            });
+        if (fileOptions.relativeTo === undefined && (fileOptions.root || file.path)) {
+          fileOptions.relativeTo = path.dirname(path.resolve(options.root || file.path));
         }
-    });
+
+        tpl2js.inline(buf, fileOptions || {}, function (err, result) {
+
+          if (err) {
+            done(err);
+            return;
+          }
+
+          done(null, new Buffer(result));
+        });
+      });
+
+      run(file, function (err, contents) {
+
+        if (err) {
+          self.emit('error', new PluginError('gulp-angular-tpl2js', err, {fileName: file.path}));
+        } else {
+          file.contents = contents;
+          self.push(file);
+        }
+        cb();
+      });
+    }
+  });
 };
--- test/test.js
+++ test/test.js
@@ -9,105 +9,105 @@ var expect = chai.expect;
 chai.should();
 
 String.prototype.min = function () {
-    return this.toString().replace(/ /g, '').replace(/(\r\n|\n|\r)/gm, '');
-}
+  return this.toString().replace(/ /g, '').replace(/(\r\n|\n|\r)/gm, '');
+};
 
 describe('gulp-angular-tpl2js: init', function () {
 
-    it('should return the gulp-angular-tpl2js object: required export', function () {
-        expect(tpl2js).to.be.function;
-    });
+  it('should return the gulp-angular-tpl2js object: required export', function () {
+    expect(tpl2js).to.be.a('function');
+  });
 });
 
 describe('gulp-angular-tpl2js: base functionality', function () {
 
-    it('should allow the file through', function (done) {
-        var i = 0;
+  it('should allow the file through', function (done) {
+    var i = 0;
 
-        gulp.src('test/fixtures/js/directive.js')
-            .pipe(tpl2js())
-            .on('data', function (file) {
-                i += 1;
-            })
-            .once('end', function () {
-                i.should.equal(1);
-                done();
-            });
+    gulp.src('test/fixtures/js/directive.js')
+      .pipe(tpl2js())
+      .on('data', function (file) {
+        i += 1;
+      })
+      .once('end', function () {
+        i.should.equal(1);
+        done();
+      });
+  });
+
+  it('should produce the expected file', function (done) {
+    var expected = 'angular.module(\'mod\').directive(\'dir\', function () {' +
+      'return {' +
+      'scope: {},' +
+      'template: \'<span>basic {{ stuff }}</span>\',' +
+      'link: function (scope, elem, attrs) {' +
+      '}' +
+      '}' +
+      '});';
+
+    var actual;
+
+    gulp.src('test/fixtures/js/directive.js')
+      .pipe(tpl2js())
+      .on('data', function (file) {
+        actual = file.contents.toString();
+      })
+      .once('end', function () {
+        expect(actual.min()).to.equal(expected.min());
+        done();
+      });
+  });
+
+  it('should produce the expected files: multiple', function (done) {
+    var expected1 = 'angular.module(\'mod\').directive(\'dir\', function () {' +
+      'return {' +
+      'scope: {},' +
+      'template: \'<span>basic {{ stuff }}</span>\',' +
+      'link: function (scope, elem, attrs) {' +
+      '}' +
+      '}' +
+      '});';
+
+    var expected2 = 'angular.module(\'mod\').directive(\'dir2\', function () {' +
+      'return {' +
+      'scope: {},' +
+      'template: \'<div>like woah</div>\',' +
+      'link: function (scope, elem, attrs) {' +
+      '}' +
+      '}' +
+      '});';
+
+    var actual1, actual2;
+
+    gulp.src('test/fixtures/js/*.js')
+      .pipe(tpl2js())
+      .on('data', function (file) {
+
+        if (!actual1)
+          actual1 = file.contents.toString();
+        else
+          actual2 = file.contents.toString();
+      })
+      .once('end', function () {
+        expect(actual1.min()).to.equal(expected1.min());
+        expect(actual2.min()).to.equal(expected2.min());
+        done();
+      });
+  });
+
+  it('should force a plugin error', function (done) {
+
+    var js = new File({
+      path: '/',
+      contents: new Buffer('angular.module(\'mod\').directive(\'dir\', function () { return { scope: {}, templateUrl: \'fixtures/templates/404.html\', link: function (scope, elem, attrs) { } } });')
     });
 
-    it('should produce the expected file', function (done) {
-        var expected = 'angular.module(\'mod\').directive(\'dir\', function () {' +
-            'return {' +
-            'scope: {},' +
-            'template: \'<span>basic {{ stuff }}</span>\',' +
-            'link: function (scope, elem, attrs) {' +
-            '}' +
-            '}' +
-            '});';
-
-        var actual;
-
-        gulp.src('test/fixtures/js/directive.js')
-            .pipe(tpl2js())
-            .on('data', function (file) {
-                actual = file.contents.toString();
-            })
-            .once('end', function () {
-                expect(actual.min()).to.equal(expected.min());
-                done();
-            });
-    });
-
-    it('should produce the expected files: multiple', function (done) {
-        var expected1 = 'angular.module(\'mod\').directive(\'dir\', function () {' +
-            'return {' +
-            'scope: {},' +
-            'template: \'<span>basic {{ stuff }}</span>\',' +
-            'link: function (scope, elem, attrs) {' +
-            '}' +
-            '}' +
-            '});';
-
-        var expected2 = 'angular.module(\'mod\').directive(\'dir2\', function () {' +
-            'return {' +
-            'scope: {},' +
-            'template: \'<div>like woah</div>\',' +
-            'link: function (scope, elem, attrs) {' +
-            '}' +
-            '}' +
-            '});';
-
-        var actual1, actual2;
-
-        gulp.src('test/fixtures/js/*.js')
-            .pipe(tpl2js())
-            .on('data', function (file) {
-
-                if (!actual1)
-                    actual1 = file.contents.toString();
-                else
-                    actual2 = file.contents.toString();
-            })
-            .once('end', function () {
-                expect(actual1.min()).to.equal(expected1.min());
-                expect(actual2.min()).to.equal(expected2.min());
-                done();
-            });
-    });
-
-    it('should force a plugin error', function (done) {
-
-        var js = new File({
-            path: '/',
-            contents: new Buffer('angular.module(\'mod\').directive(\'dir\', function () { return { scope: {}, templateUrl: \'fixtures/templates/404.html\', link: function (scope, elem, attrs) { } } });')
-        });
-
-        vfsFake.src(js)
-            .pipe(tpl2js())
-            .on('error', function (err) {
-                expect(err).to.exist;
-                expect(err).to.be.an.instanceof(PluginError);
-                done();
-            });
-    });
+    vfsFake.src(js)
+      .pipe(tpl2js())
+      .on('error', function (err) {
+        expect(err).to.exist;
+        expect(err).to.be.an.instanceof(PluginError);
+        done();
+      });
+  });
 });
